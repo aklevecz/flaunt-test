a549764c1812093c2b181ab08784bb5c26308b86
aklevecz <teh@raptor.pizza>
2022-02-26 09:13:51 -0800

    ad

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9539184273156609"
+            crossorigin="anonymous"
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
